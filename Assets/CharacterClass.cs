bed0e9bf0fb3adf45ecd4cd51cd2927500dc0432
Syrko <kostassir@yahoo.gr>
2020-02-07 22:21:40 +0200

    Implemented basic character class

--- Assets/CharacterClass.cs
+++ Assets/CharacterClass.cs
@@ -5,19 +5,47 @@ namespace Assets
 {
     class CharacterClass
     {
+        private static readonly List<DndClass> Classes = new List<DndClass>()
+        {
+            new DndClass("Fighter", DieType.D10),
+            new DndClass("Rogue", DieType.D8)
+        };
+
         private int level;
         private string name;
-        private int id;
-        private int hitDie;
+        private Die hitDie;
         private List<int> proficiencies;
         private List<int> features;
 
+        public CharacterClass(int level, string name, Die hitDie, List<int> proficiencies, List<int> features)
+        {
+            this.level = level;
+            this.name = name;
+            this.hitDie = hitDie;
+            this.proficiencies = proficiencies;
+            this.features = features;
+        }
+
+        public int Level { get => level; }
+        public string Name { get => name; }
+        public Die HitDie { get => hitDie; }
+        public List<int> Proficiencies { get => proficiencies; }
+        public List<int> Features { get => features; }
         
-        public int Level { get => level; set => level = value; }
-        public string Name { get => name; set => name = value; }
-        public int Id { get => id; set => id = value; }
-        public int HitDie { get => hitDie; set => hitDie = value; }
-        public List<int> Proficiencies { get => proficiencies; set => proficiencies = value; }
-        public List<int> Features { get => features; set => features = value; }
+        public void LevelUp() { level++; }
+        public void AddProficiency(int newProficiency) { proficiencies.Add(newProficiency); }
+        public void AddFeature(int newFeature) { features.Add(newFeature); }
+    }
+
+    struct DndClass
+    {
+        public string Name { get; }
+        public string HitDie { get; }
+
+        public DndClass(string Name, string HitDie)
+        {
+            this.Name = Name;
+            this.HitDie = HitDie;
+        }
     }
 }
