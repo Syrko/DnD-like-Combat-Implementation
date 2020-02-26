47157277ad287e1fa76186a38f9f482ea7ee88f5
Syrko <kostassir@yahoo.gr>
2020-02-27 00:36:44 +0200

    First try at implementing classes

---
+++ Assets/Combat/VanillaRogue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Combat
+{
+    class VanillaRogue: Rogue
+    {
+        VanillaRogue()
+        {
+            name = "Rogue";
+            hitDie = new Die(DieType.D8);
+            this.level = 1;
+        }
+    }
+}
