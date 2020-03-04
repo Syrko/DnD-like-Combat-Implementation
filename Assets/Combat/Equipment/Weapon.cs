c4a006a150cd7d67b36691ade3233a9691de2e0d
Syrko <kostassir@yahoo.gr>
2020-03-04 15:06:38 +0200

    Organized files into folders

---
+++ Assets/Combat/Equipment/Weapon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Combat
+{
+    public abstract class Weapon : Equipment
+    {
+        private int damage;
+        private int spdFactorModifier;
+        private string name;    //TODO change diagram -- fix name in diagram
+        private int range;
+        private string type;    // TODO change diagram -- change type
+
+        public int RollForAttack()
+        {
+            
+        }
+    }
+}
