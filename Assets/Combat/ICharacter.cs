9683aac3194fe2864c9493d167a7572658625164
Syrko <kostassir@yahoo.gr>
2020-03-04 14:36:26 +0200

    Added more methods

--- Assets/Combat/ICharacter.cs
+++ Assets/Combat/ICharacter.cs
@@ -9,5 +9,9 @@ namespace Assets.Combat
     interface ICharacter
     {
         public void LevelUp();
+        public void Move(int distance);
+        public void UseSkill();
+        public void Attack(Weapon weapon, Character target);
+        public void CastSpell();
     }
 }
