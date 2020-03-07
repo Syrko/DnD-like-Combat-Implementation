558758e78369c38bd3f66152ce63b51227f83a42
NikolasTol <nikolas.tolis@gmail.com>
2020-03-07 19:19:06 +0200

    Added properties

--- Assets/Combat/CharacterGeneral/Race.cs
+++ Assets/Combat/CharacterGeneral/Race.cs
@@ -13,5 +13,11 @@ namespace Assets.Combat
         private Dictionary<string, int> abilityScoreDecrease;
         private int speed;
         private List<string> languages;
+
+        public string Name { get => name; set => name = value; }
+        public Dictionary<string, int> AbilityScoreIncrease { get => abilityScoreIncrease; set => abilityScoreIncrease = value; }
+        public Dictionary<string, int> AbilityScoreDecrease { get => abilityScoreDecrease; set => abilityScoreDecrease = value; }
+        public int Speed { get => speed; set => speed = value; }
+        public List<string> Languages { get => languages; set => languages = value; }
     }
 }
