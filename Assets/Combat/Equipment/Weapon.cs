a3d369ef2ace59a40ca96c460b5264d5b08d72c3
NikolasTol <nikolas.tolis@gmail.com>
2020-03-07 15:55:45 +0200

    Updated Weapon Class

--- Assets/Combat/Equipment/Weapon.cs
+++ Assets/Combat/Equipment/Weapon.cs
@@ -8,7 +8,7 @@ namespace Assets.Combat
 {
     public abstract class Weapon : Equipment
     {
-        // TODO change diagram --  update class
+        // TODO change diagram -- update class
         private Die dieDamage;
         private int numberOfDice; 
         private string name;    //TODO change diagram -- fix name in diagram
@@ -16,11 +16,12 @@ namespace Assets.Combat
         private bool isMartial;    // TODO change diagram -- change type
         private bool isVersatile;
 
-        public int NumberOfDice { get => numberOfDice; set => numberOfDice = value; }
-        public string Name { get => name; set => name = value; }
-        public int Range { get => range; set => range = value; }
-        public bool IsMartial { get => isMartial; set => isMartial = value; }
-        public bool IsVersatile { get => isVersatile; set => isVersatile = value; }
+        // TODO change diagram -- change access modifier
+        internal int NumberOfDice { get => numberOfDice; set => numberOfDice = value; }
+        internal string Name { get => name; set => name = value; }
+        internal int Range { get => range; set => range = value; }
+        internal bool IsMartial { get => isMartial; set => isMartial = value; }
+        internal bool IsVersatile { get => isVersatile; set => isVersatile = value; }
         internal Die DieDamage { get => dieDamage; set => dieDamage = value; }
     }
 }
