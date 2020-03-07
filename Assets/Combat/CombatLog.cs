bc552706aec2b60227caf5d7f5eab6dc708068b8
Syrko <kostassir@yahoo.gr>
2020-03-07 19:35:10 +0200

    Created CombatLog class

---
+++ Assets/Combat/CombatLog.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatLog : MonoBehaviour
+{
+    private Queue<string> messageQueue;
+
+    void Awake()
+    {
+        messageQueue = new Queue<string>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while(messageQueue.Count > 0)
+        {
+            string msg = messageQueue.Dequeue();
+            // TODO print msg
+        }
+    }
+
+    internal void AddMessageToQueue(string msg)
+    {
+        messageQueue.Enqueue(msg);
+    }
+}
