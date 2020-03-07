b356ad6f8c7ee3bbad68e7f81455712ce44999fb
Syrko <kostassir@yahoo.gr>
2020-03-07 18:42:36 +0200

    Changed name to characterName

--- Assets/Combat/CharacterGeneral/Character.cs
+++ Assets/Combat/CharacterGeneral/Character.cs
@@ -18,14 +18,14 @@ public class Character : MonoBehaviour, IDamageable, IKillable, ICharacter
     private int ac;
     private int initiative;
     private int speed;
-    private string name;
+    private string characterName;
     private int xp;
 
     public HitPoints HitPoints { get => hitPoints; set => hitPoints = value; }
     public int AC { get => ac; set => ac = value; }
     public int Initiative { get => initiative; set => initiative = value; }
     public int Speed { get => speed; set => speed = value; }
-    public string Name { get => name; set => name = value; }
+    public string CharacterName { get => characterName; set => characterName = value; }
     public int XP { get => xp; set => xp = value; }
     public bool PlayerControlled { get => playerControlled; set => playerControlled = value; }
     public int Proficiency { get => proficiency; set => proficiency = value; }
